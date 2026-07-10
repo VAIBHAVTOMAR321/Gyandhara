40bd9a8d428161e03932f102a13b5091bf02cfb5
Deepika Chauhan <deepikachauhan8988@gmail.com>
2026-07-10 16:27:02 +0530

    updated

--- src/components/admin_panel/SchoolQuiz.jsx
+++ src/components/admin_panel/SchoolQuiz.jsx
@@ -604,7 +604,7 @@ const SchoolQuiz = () => {
                             <tr>
                               <th className="py-3 px-2">Title</th>
                               <th className="py-3 px-2">Activity Type</th>
-                              <th className="py-3 px-2">Classes</th>
+                              <th className="py-3 px-2">Course</th>
                               <th className="py-3 px-2">Institutions</th>
                               <th className="py-3 px-2">Participants</th>
                               <th className="py-3 px-2">Questions</th>
@@ -740,7 +740,7 @@ const SchoolQuiz = () => {
 
                                 <div className="mb-2">
                                   <small className="text-muted d-block">
-                                    Classes:
+                                    Course:
                                   </small>
                                   <div>
                                     {quiz.class_allowed?.map((c) => (
